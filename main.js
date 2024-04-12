42579af22132ef8ec605ab729d05e99bb16144db
Ezekiel <realnyamasege2@gmail.com>
2024-04-12 08:46:00 +0300

    add main.js

--- main.js
+++ main.js
@@ -1,20 +1,63 @@
-const baseUrl = "http://localhost:4000";
+const baseUrl = " http://localhost:3000";
+const box = document.querySelector(".box");
+const container = document.querySelector(".container");
+const form = document.querySelector("#submit-form");
 
-const painters = {
-  name: "Elon",
-  age: "25",
-  country: "USA",
-  profession: "Painter",
-};
-
-
-
-function fetchPainters() {
-  fetch(`${baseUrl}/painters`)
-    .then((response) => response.json())
-    .then((data) => {
-        data.map((painters) => console.log(painters.name.toUpperCase()) )
+function fetchImages() {
+  fetch(`${baseUrl}/images`)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (images) {
+      console.log(images);
+      images.map(function (image) {
+        const artCard = `<div class="box" id="box">
+      <img src=${image.imageUrl} alt="art image" />
+      <button>${image.genre}</button>
+      <button onclick="deleteArtWork(${image.id})">delete art</button>
+      </div>`;
+        container.insertAdjacentHTML("beforeend", artCard);
+      });
     });
 }
 
-fetchPainters();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formTarget = event.target;
+  const artist = formTarget.artist.value;
+  const genre = formTarget.genre.value;
+  const imageUrl = formTarget.image.value;
+
+  submitArtwork(artist, genre, imageUrl);
+});
+
+function submitArtwork(artist, genre, imageUrl) {
+  const art = {
+    artist: artist,
+    genre: genre,
+    imageUrl: imageUrl,
+  };
+  fetch(`${baseUrl}/images`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(art),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert("Artwork Posted successfully");
+    });
+
+}
+
+function deleteArtWork(id) {
+  fetch(`${baseUrl}/images/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      alert("Art deleted!");
+    });
+}
+fetchImages();
